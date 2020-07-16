0a08e751d467385434459b5b7be0422fa075192e
Jesus Rodrguez <jesus.rodrz97@gmail.com>
2020-07-16 18:48:25 -0400

    compiled assets

--- lib/hooks.d.ts
+++ lib/hooks.d.ts
@@ -1,11 +1,33 @@
-export declare type UsePromiseParams = {
-    initialValue?: any;
-    reducer?: Function;
-};
+export interface UsePromiseOptions<T, U> {
+    onComplete?: (result: T) => void;
+    onError?: (error: U) => void;
+    skip?: boolean;
+}
+export interface UsePromiseRef<T, U> extends UsePromiseOptions<T, U> {
+    promise: () => Promise<T> | Promise<T>;
+    mounted: boolean;
+}
+export interface UsePromiseState<T, U> {
+    loading: boolean;
+    result: T | null;
+    error: U | null;
+}
+export interface UsePromiseReturn<T, U> extends UsePromiseState<T, U> {
+    call: () => Promise<T | any | null | undefined>;
+}
 /**
- * React Hook to manage promises.
- * @param {Promise} promise - The promise to be resolved.
- * @param {initialValue} initialValue -  An initial value to be hold until the promise is resolved.
+ * @param {Function} promise - A funtion that returns the promise to handle.
+ * @param {object} options - Options to set the hook.
+ * @param {Array} dependencies - Dependencies to re-run the promise automatically.
+ * @returns {object} Hook state.
  */
-declare const usePromise: (promise: Promise<any>, params: UsePromiseParams) => any[];
-export { usePromise };
+declare function usePromise<T, U>(promise: () => Promise<T>, options?: UsePromiseOptions<T, U>, dependencies?: Array<unknown>): UsePromiseReturn<T, U>;
+/**
+ * Shorthand for useEffect with an empty dependencies array.
+ * It basically executes the function once on mount and unmount.
+ *
+ * @param {Function} fn - React effect callback to run on mount and unmount.
+ * @returns {void}
+ */
+declare const useOnMount: (fn: React.EffectCallback) => void;
+export { usePromise, useOnMount };
